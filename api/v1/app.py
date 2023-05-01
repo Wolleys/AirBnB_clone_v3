9bbd078ecc9e392df834d77bd1ca93b407021be3
elvis wangari <elviswangare@gmail.com>
2023-05-02 02:50:12 +0300

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,13 @@
 """
 contains a flask application
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 
 
