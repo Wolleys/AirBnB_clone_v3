fdbbea5c37ccbac9e2da0a94351c68695359ec52
elvis wangari <elviswangare@gmail.com>
2023-05-02 02:39:24 +0300

    jsonify

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,10 +3,11 @@
 creates routes
 """
 
+from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """returns JSON status"""
-    return {"status": "OK"}
+    return jsonify({"status": "OK"})
