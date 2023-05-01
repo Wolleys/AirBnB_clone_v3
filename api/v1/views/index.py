1739ee7a99a71d787891564429605c27f3919526
elvis wangari <elviswangare@gmail.com>
2023-05-02 02:26:59 +0300

    api

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+creates routes
+"""
+
+from api.v1.views import app_views
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """returns JSON status"""
+    return {"status": "OK"}
