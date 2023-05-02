af4908373c57bc53cdb29db802fe44a59f87da71
elvis wangari <elviswangare@gmail.com>
2023-05-02 03:41:02 +0300

    new api endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ creates routes
 
 from flask import jsonify
 from models import storage
-from api.v1.views import`` app_views
+from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
